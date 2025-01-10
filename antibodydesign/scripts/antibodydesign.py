e5bf437214bdf2537539fe207942b7ec256fd67a
Bryan Briney <bryanbriney@gmail.com>
2025-01-10 13:00:49 -0800

    Update antibodydesign.py

--- antibodydesign/scripts/antibodydesign.py
+++ antibodydesign/scripts/antibodydesign.py
@@ -43,8 +43,8 @@ def cli():
 @click.option(
     "-s",
     "--seed",
-    # type=Union[int, str],
-    default=42,
+    # type=int,
+    # default=42,
     help="Random seed(s) to use, for example '42' or '42,43'. If multiple seed values are provided, each PDB file will be processed with every combination of temperature and seed.",
 )
 @click.option(
@@ -57,8 +57,8 @@ def cli():
 @click.option(
     "-t",
     "--temperature",
-    # type=Union[float, str],
-    default=0.1,
+    # type=float,
+    # default=0.1,
     help="Temperature(s) to use, for example '0.1' or '0.1,0.2'. If multiple temperature values are provided, each PDB file will be processed with every combination of temperature and seed.",
 )
 @click.option(
