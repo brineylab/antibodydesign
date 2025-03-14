a5cf88638783c1b8ae1781bb643057e788874767
Bryan Briney <bryanbriney@gmail.com>
2025-03-14 14:21:07 -0700

    Update protenix.py

--- mabmaker/tools/protenix.py
+++ mabmaker/tools/protenix.py
@@ -67,9 +67,7 @@ def protenix(
     futures = []
     with cf.ThreadPoolExecutor(max_workers=num_gpus) as executor:
         for run in runs:
-            run_output_dir = os.path.join(output_path, run.name)
-            abutils.io.make_dir(run_output_dir)
-            cmd = _build_protenix_command(run, run_output_dir, use_msa_server)
+            cmd = _build_protenix_command(run, output_path, use_msa_server)
             futures.append(executor.submit(gpu_worker, cmd, gpu_queue))
 
         # monitor progress
